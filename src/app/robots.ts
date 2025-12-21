467e8cb1a5bf7ec9ea07431b0730114c9a936172
Tarandro <contact@tarandro.org>
2025-12-21 21:34:48 +0100

    Fix: Correct robots.txt format to allow proper indexation

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,13 +2,11 @@ import { MetadataRoute } from 'next'
  
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/api/', '/admin/'],
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/admin/'],
+    },
     sitemap: 'https://tarandro.org/sitemap.xml',
   }
 }
